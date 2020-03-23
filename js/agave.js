16828ba1a4e4b0a2529eee28f881009776ac0f10
unknown <sleepipanda.dev@gmail.com>
2020-03-22 23:47:41 -0500

    Co-authored-by: Scott Everitt <saeveritt@gmail.com> Co-authored-by: Jack <jack.day.27@gmail.com> Co-authored-by: Andrew Clark <drewboy879@gmail.com>

--- js/agave.js
+++ js/agave.js
@@ -1,5 +1,8 @@
+// Issue Modes Array
 var Issue_Modes = ["None", "Custom", "Once", "Multi", "Mono", "Singlet", "Unflushable", "Subscription"]
-
+// Network Arrays
+var Networks = {"Peercoin": "Peercoin", "Peercoin-Testnet": "PeercoinTestnet","Bitcoin": "Bitcoin", "Bitcoin-Testnet":"BitcoinTestnet"}
+// Render Pages
 var RENDERERS = {"LOGIN":render_loginPage, "OVERVIEW":render_overview}
 
 ////////////////////////////////////////////////////////
@@ -60,23 +63,66 @@ function render_loginPage(){
   var builder = new DocumentFragment();
 
   // Parent Login Div
+
+  var login_div_container = document.createElement("div")
+  login_div_container.className="login_form__container"
+
   var login_div = document.createElement("div")
-  login_div.className+= "loginForm"
+  login_div.className+= "login_form"
 
   var phraseInput = document.createElement("input")
+  phraseInput.className+= " login_form__phrase_entry"
+  phraseInput.className+= " login_form__input"
+  phraseInput.id+="mnemonic-entry"
   phraseInput.placeholder+= "12-Word Passphrase"
   login_div.appendChild(phraseInput)
 
   var passwordInput = document.createElement("input")
+  passwordInput.id += "safety-code"
+  //Why no += in this specific case?
+  passwordInput.type = "password"
+  passwordInput.className+= " login_form__input"
   passwordInput.placeholder+= "Password"
   login_div.appendChild(passwordInput)
 
-  var loginButton = document.createElement("button")
-  loginButton.innerText += "Login"
-  login_div.appendChild(loginButton)
+  var loginButton = document.createElement("input")
+  loginButton.className+= " login_form__button"
+  loginButton.setAttribute("type","button");
+  loginButton.value = "Login";
+  loginButton.addEventListener("click",function(event){
+    console.log("clicked login")
+    safety_code = document.getElementById("safety-code").value
+    mnemonic = document.getElementById("mnemonic-entry").value
+    address = getAddress(mnemonic,"peercoinTestnet")
+    lockedKey = encryptData( safety_code, getWIF(mnemonic))
+    sessionStorage.setItem("address",address)
+    sessionStorage.setItem("lockedKey",lockedKey)
+    location.reload()
+  })
+
+  var generateButton = document.createElement("input")
+  generateButton.setAttribute("type","button");
+  generateButton.className="login_form__button"
+  generateButton.className+= " login_form__button"
+  generateButton.value = "Generate";
+  generateButton.addEventListener("click",function(event){
+    mnemonic = getMnemonic();
+    console.log(mnemonic)
+    document.getElementById("mnemonic-entry").value=mnemonic
+  })
+
+  var button_row = document.createElement("div")
+  button_row.className="login_form__button_row"
+
+  
+  button_row.appendChild(loginButton)
+  button_row.appendChild(generateButton)
+
+  login_div.appendChild(button_row)
+  login_div_container.appendChild(login_div)
 
   // Build it all
-  builder.appendChild(login_div)
+  builder.appendChild(login_div_container)
 
   var main_div = getMainDiv()
   clearObjectHTML(main_div)
@@ -103,10 +149,10 @@ function render_not_implemented(name){
   builder.appendChild(not_implemented_div)
  
 
-  var main_div = getMainDiv()
-  clearObjectHTML(main_div)
-  main_div.appendChild(builder)
-}
+//   var main_div = getMainDiv()
+//   clearObjectHTML(main_div)
+//   main_div.appendChild(builder)
+ }
 
 
 
@@ -193,12 +239,48 @@ function createTransactionDiv(transaction, transID){
 }
 
 addOnClickToNavItems()
-var page = document.location.hash.replace("#","")
-if (page != ""){
-  render_page(page.toUpperCase())
-}
-else {
-  render_page("MENU")
+
+// Check if user has logged in by verifying the sessionStorage keys are not null
+var isLoggedIn = function() {
+  address = sessionStorage.getItem('address');
+  lockedKey = sessionStorage.getItem('lockedKey');
+  if (address && lockedKey == lockedKey){
+      console.log(true)
+      // Add the address
+      document.getElementById('address').innerHTML = address;
+      // Create an Identicon for the address
+      //overview_identicon_canvas.setAttribute("data-jdenticon-value",transID)
+      //<canvas data-jdenticon-value="" id="address-identicon" class="user-nav__icon">
+      document.getElementById("address-identicon").setAttribute("data-jdenticon-value", transID)
+      return true;
+  } else{
+      return false;
+  }
 }
 
+var logoutUser = function(){
+  logout = document.getElementById("logout-key")
+  logout.addEventListener("click",function(event){
+     sessionStorage.clear()
+     location.reload()
+   })
+ }
 
+// var page = document.location.hash.replace("#","")
+// if (page != "") && ( isLoggedIn() ){
+//   render_page(page.toUpperCase())
+// }else { 
+
+
+if ( !isLoggedIn() ) {
+    // If user is not logged in then load the login page
+    console.log("User is not logged in")
+    render_page("LOGIN")
+  } else if ( isLoggedIn() ) {
+    page = document.location.hash.replace("#","")
+    if (page === ""){
+      render_page("OVERVIEW")
+    }else{
+      render_page(page.toUpperCase())
+  }
+}
